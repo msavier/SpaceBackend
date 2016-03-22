d076dccbc20972035ca403077fd25eed50286778
GabeF <gabrielfrischer@gmail.com>
2016-03-22 11:43:37 -0700

    3/22/16 Added Private Space Form and Logo on SideNav

--- js/init.js
+++ js/init.js
@@ -52,3 +52,118 @@ $(document).ready(function(){
     $(document).ready(function() {
     $('select').material_select();
   });
+
+
+var ItemSet = {
+
+  create: function() {
+
+    var inst = Object.create(this);
+
+    inst.events = {};
+    inst.items = [];
+    
+    return inst;
+
+  },
+
+  add: function( item ) {
+
+    this.items.push( item );
+
+    this.emit('add' );
+    this.emit('change' );
+  },
+
+  remove: function( item ) {
+
+    this.items.splice( this.items.indexOf( item ), 1 );
+
+    this.emit( 'remove' );
+    this.emit( 'change' );
+  },
+
+  contains: function( item ) {
+
+    return ( this.items.indexOf( item ) > -1 );
+
+  },
+  
+  sort: function( func ) {
+    this.items.sort( func );
+    
+    this.emit( 'sort' );
+    this.emit( 'change' );
+  },
+  
+  modify: function( func ) {
+    func.call( this );
+    this.emit( 'change' );
+  },
+
+  on: function( evt, func ) {
+
+    if ( !this.events[ evt ] ) this.events[ evt ] = [];
+    this.events[ evt ].push( func );
+
+  },
+
+  emit: function( evt ) {
+
+    var evts = this.events[ evt ];
+    if ( evts ) {
+
+      for ( var i = 0, l = evts.length; i < l; i++ ) {
+        evts[ i ]( this );
+      }
+
+    }
+
+  }
+};
+
+
+
+
+var emailList = ItemSet.create();
+
+
+
+$('#email_adder').on( 'submit', function( e ) {
+  var $email = $('#add_email');
+  
+  if( $email.val().trim() ) {
+    emailList.add( $email.val()  );
+    $email.val('');
+    $email.focus();
+  }
+  
+  e.preventDefault();
+});
+
+
+$('#email_list').on( 'click', '.email', function( e ) {
+  emailList.remove( $(this).html() );
+});
+
+$('#sort').on( 'click', function( e ) {
+   emailList.sort();
+});
+
+$('#pop').on( 'click', function( e ) {
+   emailList.modify(function(){
+     this.items.pop();
+   });
+});
+
+emailList.on('change', function() {
+  var $nodes = [];
+
+  for (var i = 0, l = emailList.items.length; i < l; i++) {
+    $nodes.push('<div class="email">' + emailList.items[i] + '</div>');
+  }
+
+  $('#email_list').html($nodes);
+
+});
+
